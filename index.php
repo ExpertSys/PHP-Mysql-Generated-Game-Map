ae74531e5058a35f1b5ec9f108283f757fd228b9
Jordan Brown <ampzgunz@hotmail.com>
2017-11-23 23:18:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="css/style.css"/>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
+<script type="text/javascript">
+</script>
 </head>
 <body>
 
