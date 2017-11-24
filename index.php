c466e5b8e6723b4ba9bae983072f2e3810bcf908
Jordan Brown <ampzgunz@hotmail.com>
2017-11-23 23:18:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     require_once("inc/db.php");
     require_once("inc/map.php");
 
-    $db = new Database();
     $map = new Map($_POST['x'], $_POST['y']);
     $map->createMap();
     $map->battle();
@@ -11,12 +10,8 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="css/style.css"/>
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/index.js"></script>
-<script type="text/javascript">
-</script>
 </head>
 <body>
-<meta http-equiv="refresh" content="3">
+
 </body>
 </html>
